5b9fbbe14598211f3310deb8bfec3bbf2f0915fe
Rohan Singh <40713164+tagrohan@users.noreply.github.com>
2021-07-23 22:24:31 +0530

    Update Config.java

--- app/src/main/java/com/luqidoproit/educaderpro/Config.java
+++ app/src/main/java/com/luqidoproit/educaderpro/Config.java
@@ -6,5 +6,6 @@ public class Config {
 	public static final String DEVELOPER_KEY = "AIzaSyA60BDL4qE5zPlej1Wq1jcrpcpaD5hl1us";
 	
 	// YouTube video id
+	// todo need to be updated
 	public static final String YOUTUBE_VIDEO_CODE = "6LV3JdR5aZw";
 }
